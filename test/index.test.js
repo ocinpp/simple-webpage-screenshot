65844e1ab3bca4723170550e511463ec3442a513
ocinpp <ocin25@gmail.com>
2020-03-15 20:34:54 +0800

    Change test threshold to 5

--- test/index.test.js
+++ test/index.test.js
@@ -65,7 +65,7 @@ describe("Checking capture", () => {
           null,
           screenCapture.width,
           screenCapture.height,
-          { threshold: 0.3 }
+          { threshold: 0.5 }
         );
 
         expect(diffPixels).toBe(0);
